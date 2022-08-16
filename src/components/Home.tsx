3a5b2848b4888b18b7ebeecfe6b40dbbafa7edd8
Mac_Developer <araozangel842@gmail.com>
2022-08-16 15:06:06 +0300

    Testing Netlify deploy

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <h2>Home</h2>
+      <h2>Home side</h2>
       <button onClick={() => Auth.signOut()}>Go to Out</button>
     </div>
   )
