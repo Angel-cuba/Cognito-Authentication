f934a49e01d6f8d9c6c03e3e7982e95a3358dee2
Mac_Developer <araozangel842@gmail.com>
2022-08-16 19:12:54 +0300

    Fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,43 @@ import { Route, Routes } from 'react-router-dom';
 // import Initial from './components/Initial';
 import Home from './components/Home';
 import GoOut from './components/GoOut';
-//Amplify 
-import { Amplify } from 'aws-amplify';
+//Amplify
+import { Amplify, API, graphqlOperation } from 'aws-amplify';
 import awsExports from './aws-exports';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import './App.css';
-import '@aws-amplify/ui-react/styles.css'
+import '@aws-amplify/ui-react/styles.css';
+//GraphQL Operation
+import { listVideos } from './graphql/queries';
+
+
+//Initialization of Amplify
 Amplify.configure(awsExports);
 
 function App() {
+  const [videos, setVideos] = React.useState([]);
+  console.log('videos',videos);
+  const fetchVideo = async () => {
+    try {
+      const videoData: any = await API.graphql(graphqlOperation(listVideos));
+
+      // const list = videoData.listVideos.Items 
+      // console.log('Videos',videoData.data.listVideos.Items);
+      setVideos(videoData.data.listVideos.Items);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+React.useEffect(() => {
+  fetchVideo();
+} , []);
   return (
     <div className="App">
-     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/out" element={<GoOut />} />
-      {/* <Route path="/" element={<Initial />} /> */}
-     </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/out" element={<GoOut />} />
+        {/* <Route path="/" element={<Initial />} /> */}
+      </Routes>
     </div>
   );
 }
